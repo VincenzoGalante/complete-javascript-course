f9443324497221dca56201dba8b231ede8c4406e
Vincenzo Galante <mr.vincenzo.galante@gmail.com>
2022-04-01 19:55:31 +0200

    Started 9

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,30 @@ const restaurant = {
     },
   },
 };
+
+let arr = [2, 3, 4];
+const a = [0];
+const b = [1];
+const c = [2];
+
+let [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr); // not modifying array
+
+let [first, , second] = restaurant.categories; // skipping variables which are not needed
+console.log(first, second);
+
+[first, second] = [second, first]; // switching variables the easiest
+
+// Receiving two variables from one function call, 2 new variables
+const [starter, mainCourse] = restaurant.order(1, 1);
+console.log(starter, mainCourse);
+
+// destructuring in nested arrays
+const nested = [2, 3, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// default values
+const [one = 1, two = 1, three = 1] = [1, 2];
+console.log(one, two, three);
